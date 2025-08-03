c94f6f8a40e6ffc8200373049a8b6e44bc2133c2
luwei <lwpk110@outlook.com>
2025-08-03 13:01:26 +0800

    refactor: update datanode container

--- internal/controller/data/container/wait_for_namenodes.go
+++ internal/controller/data/container/wait_for_namenodes.go
@@ -1,11 +1,16 @@
 package container
 
 import (
+	"maps"
+	"strings"
+
 	hdfsv1alpha1 "github.com/zncdatadev/hdfs-operator/api/v1alpha1"
 	"github.com/zncdatadev/hdfs-operator/internal/common"
 	"github.com/zncdatadev/hdfs-operator/internal/constant"
 	commonsv1alpha1 "github.com/zncdatadev/operator-go/pkg/apis/commons/v1alpha1"
+	"github.com/zncdatadev/operator-go/pkg/constants"
 	"github.com/zncdatadev/operator-go/pkg/reconciler"
+	oputil "github.com/zncdatadev/operator-go/pkg/util"
 	corev1 "k8s.io/api/core/v1"
 )
 
@@ -14,7 +19,7 @@ type WaitForNameNodesContainerBuilder struct {
 	instance        *hdfsv1alpha1.HdfsCluster
 	roleGroupInfo   *reconciler.RoleGroupInfo
 	roleGroupConfig *commonsv1alpha1.RoleGroupConfigSpec
-	image           *hdfsv1alpha1.ImageSpec
+	image           *oputil.Image
 }
 
 // NewWaitForNameNodesContainerBuilder creates a new wait-for-namenodes container builder
@@ -22,7 +27,7 @@ func NewWaitForNameNodesContainerBuilder(
 	instance *hdfsv1alpha1.HdfsCluster,
 	roleGroupInfo *reconciler.RoleGroupInfo,
 	roleGroupConfig *commonsv1alpha1.RoleGroupConfigSpec,
-	image *hdfsv1alpha1.ImageSpec,
+	image *oputil.Image,
 ) *WaitForNameNodesContainerBuilder {
 	return &WaitForNameNodesContainerBuilder{
 		instance:        instance,
@@ -34,24 +39,19 @@ func NewWaitForNameNodesContainerBuilder(
 
 // Build builds the wait-for-namenodes container
 func (b *WaitForNameNodesContainerBuilder) Build() *corev1.Container {
-	// Convert ImageSpec to container string
-	imageString := b.image.Repo + "/hadoop:" + b.image.ProductVersion
+	// Create the common container builder
+	builder := common.NewHdfsContainerBuilder(
+		constant.WaitForNameNodesComponent,
+		b.image,
+		b.instance.Spec.ClusterConfig.ZookeeperConfigMapName,
+		b.roleGroupInfo,
+		b.roleGroupConfig,
+	)
 
-	// Create wait-for-namenodes component implementation
+	// Create wait-for-namenodes component and build container
 	component := newWaitForNameNodesComponent(b.instance, b.roleGroupInfo)
 
-	// Build the container manually
-	container := &corev1.Container{
-		Name:            component.GetContainerName(),
-		Image:           imageString,
-		Command:         component.GetCommand(),
-		Args:            component.GetArgs(),
-		Env:             component.GetEnvVars(),
-		VolumeMounts:    component.GetVolumeMounts(),
-		ImagePullPolicy: b.image.PullPolicy,
-	}
-
-	return container
+	return builder.BuildWithComponent(component)
 }
 
 // WaitForNameNodesComponent implements the component interface for wait-for-namenodes
@@ -79,25 +79,75 @@ func (c *WaitForNameNodesComponent) GetCommand() []string {
 }
 
 func (c *WaitForNameNodesComponent) GetArgs() []string {
-	return []string{
-		"echo 'Waiting for NameNodes to be ready...' && sleep 10 && echo 'NameNodes should be ready now'",
-	}
+	tmpl := `mkdir -p /kubedoop/config/wait-for-namenodes
+cp /kubedoop/mount/config/wait-for-namenodes/*.xml /kubedoop/config/wait-for-namenodes
+cp /kubedoop/mount/config/wait-for-namenodes/wait-for-namenodes.log4j.properties /kubedoop/config/wait-for-namenodes/log4j.properties
+
+{{ if .kerberosEnabled }}
+{{- .kerberosEnv }}
+
+{{- .kinitScript }}
+{{- end }}
+
+echo "Waiting for namenodes to get ready:"
+n=0
+while [ ${n} -lt 12 ];
+do
+    ALL_NODES_READY=true
+    for namenode_id in ` + c.nameNodeIds() + `;
+    do
+        echo -n "Checking pod $namenode_id... "
+        SERVICE_STATE=$(/kubedoop/hadoop/bin/hdfs haadmin -getServiceState $namenode_id | tail -n1 || true)
+        if [ "$SERVICE_STATE" = "active" ] || [ "$SERVICE_STATE" = "standby" ]; then
+            echo "$SERVICE_STATE"
+        else
+            echo "not ready"
+            ALL_NODES_READY=false
+        fi
+    done
+    if [ "$ALL_NODES_READY" == "true" ]; then
+        echo "All namenodes ready!"
+        break
+    fi
+    echo ""
+    n=$((n + 1))
+    sleep 5
+done
+`
+	data := common.CreateExportKrbRealmEnvData(c.instance.Spec.ClusterConfig)
+	principal := common.CreateKerberosPrincipal(c.instance.Name, c.instance.Namespace, constant.NameNode)
+	maps.Copy(data, common.CreateGetKerberosTicketData(principal))
+	return common.ParseTemplate(tmpl, data)
 }
 
 func (c *WaitForNameNodesComponent) GetEnvVars() []corev1.EnvVar {
-	return []corev1.EnvVar{
-		{
-			Name:  "HADOOP_CONF_DIR",
-			Value: "/stackable/config",
-		},
-	}
+	return common.GetCommonContainerEnv(c.instance.Spec.ClusterConfig, constant.WaitForNameNodesComponent)
 }
 
 func (c *WaitForNameNodesComponent) GetVolumeMounts() []corev1.VolumeMount {
-	return []corev1.VolumeMount{
+	mounts := common.GetCommonVolumeMounts(c.instance.Spec.ClusterConfig)
+	waitNameNodeMounts := []corev1.VolumeMount{
 		{
-			Name:      hdfsv1alpha1.HdfsConfigVolumeMountName,
-			MountPath: "/stackable/config",
+			Name:      hdfsv1alpha1.WaitForNamenodesConfigVolumeMountName,
+			MountPath: constants.KubedoopConfigDirMount + "/" + c.GetContainerName(),
+		},
+		{
+			Name:      hdfsv1alpha1.WaitForNamenodesLogVolumeMountName,
+			MountPath: constants.KubedoopLogDirMount + "/" + c.GetContainerName(),
 		},
 	}
+	return append(mounts, waitNameNodeMounts...)
+}
+
+func (c *WaitForNameNodesComponent) nameNodeIds() string {
+	// Get namenode role group info from the cluster
+	nameNodeRoleGroups := c.instance.Spec.NameNode.RoleGroups
+	var podNames []string
+	for groupName, roleGroupSpec := range nameNodeRoleGroups {
+		statefulSetName := c.instance.Name + "-" + string(constant.NameNode) + "-" + groupName
+		replicas := *roleGroupSpec.Replicas
+		groupPodNames := common.CreatePodNamesByReplicas(replicas, statefulSetName)
+		podNames = append(podNames, groupPodNames...)
+	}
+	return strings.Join(podNames, " ")
 }
--- internal/controller/data/statefulset_builder.go
+++ internal/controller/data/statefulset_builder.go
@@ -80,139 +80,21 @@ func (b *DataNodeStatefulSetBuilder) GetName() string {
 
 // GetMainContainers returns the main containers for datanode
 func (b *DataNodeStatefulSetBuilder) GetMainContainers() []corev1.Container {
-	containers := b.buildContainers()
-	return containers
+	return []corev1.Container{
+		b.makeDataNodeContainer(),
+	}
 }
 
 // GetInitContainers returns init containers for datanode
 func (b *DataNodeStatefulSetBuilder) GetInitContainers() []corev1.Container {
-	initContainers := b.buildInitContainers()
-	return initContainers
+	return []corev1.Container{
+		b.makeWaitForNameNodesContainer(),
+	}
 }
 
 // GetVolumes returns datanode-specific volumes
 func (b *DataNodeStatefulSetBuilder) GetVolumes() []corev1.Volume {
-	return b.buildVolumes()
-}
-
-// GetVolumeClaimTemplates returns volume claim templates for DataNode
-func (b *DataNodeStatefulSetBuilder) GetVolumeClaimTemplates() []corev1.PersistentVolumeClaim {
-	return b.buildVolumeClaimTemplates()
-}
-
-// GetSecurityContext returns security context for DataNode pods
-func (b *DataNodeStatefulSetBuilder) GetSecurityContext() *corev1.PodSecurityContext {
-	return &corev1.PodSecurityContext{
-		RunAsUser:    &[]int64{1000}[0],
-		RunAsGroup:   &[]int64{1000}[0],
-		RunAsNonRoot: &[]bool{true}[0],
-		FSGroup:      &[]int64{1000}[0],
-	}
-}
-
-// GetServiceAccountName returns service account name for DataNode
-func (b *DataNodeStatefulSetBuilder) GetServiceAccountName() string {
-	return b.instance.Name + "-datanode"
-}
-
-// NewDataNodeStatefulSetBuilder creates a new DataNode StatefulSet builder
-func NewDataNodeStatefulSetBuilder(
-	client *client.Client,
-	instance *hdfsv1alpha1.HdfsCluster,
-	roleGroupInfo *reconciler.RoleGroupInfo,
-	roleGroupConfig *commonsv1alpha1.RoleGroupConfigSpec,
-	vectorAggregatorConfigMapName string,
-) *DataNodeStatefulSetBuilder {
-	return &DataNodeStatefulSetBuilder{
-		client:                        client,
-		instance:                      instance,
-		roleGroupInfo:                 roleGroupInfo,
-		roleGroupConfig:               roleGroupConfig,
-		vectorAggregatorConfigMapName: vectorAggregatorConfigMapName,
-	}
-}
-
-// Build builds the DataNode StatefulSet
-func (b *DataNodeStatefulSetBuilder) Build(ctx context.Context) (*appsv1.StatefulSet, error) {
-	// Build DataNode containers
-	containers := b.buildContainers()
-
-	// Build init containers
-	initContainers := b.buildInitContainers()
-
-	// Create StatefulSet
-	sts := &appsv1.StatefulSet{
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      b.roleGroupInfo.GetFullName(),
-			Namespace: b.instance.Namespace,
-			Labels:    b.buildLabels(),
-		},
-		Spec: appsv1.StatefulSetSpec{
-			Replicas:    &[]int32{1}[0], // Default to 1 replica
-			ServiceName: b.roleGroupInfo.GetFullName(),
-			Selector: &metav1.LabelSelector{
-				MatchLabels: b.buildSelectorLabels(),
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: b.buildLabels(),
-				},
-				Spec: corev1.PodSpec{
-					Containers:     containers,
-					InitContainers: initContainers,
-					Volumes:        b.buildVolumes(),
-				},
-			},
-			VolumeClaimTemplates: b.buildVolumeClaimTemplates(),
-		},
-	}
-
-	return sts, nil
-}
-
-// buildContainers builds the main containers for DataNode
-func (b *DataNodeStatefulSetBuilder) buildContainers() []corev1.Container {
-	image := b.instance.Spec.Image
-
-	// Build DataNode container (passing ImageSpec directly for now)
-	dataNodeBuilder := container.NewDataNodeContainerBuilder(
-		b.instance,
-		b.roleGroupInfo,
-		b.roleGroupConfig,
-		image,
-	)
-	dataNodeContainer := dataNodeBuilder.Build()
-
-	containers := []corev1.Container{*dataNodeContainer}
-
-	// Add vector container if enabled
-	if b.vectorAggregatorConfigMapName != "" {
-		vectorContainer := b.buildVectorContainer()
-		containers = append(containers, *vectorContainer)
-	}
-
-	return containers
-}
-
-// buildInitContainers builds init containers for DataNode
-func (b *DataNodeStatefulSetBuilder) buildInitContainers() []corev1.Container {
-	image := b.instance.Spec.Image
-
-	// Build wait-for-namenodes init container
-	waitForNameNodesBuilder := container.NewWaitForNameNodesContainerBuilder(
-		b.instance,
-		b.roleGroupInfo,
-		b.roleGroupConfig,
-		image,
-	)
-	waitForNameNodesContainer := waitForNameNodesBuilder.Build()
-
-	return []corev1.Container{*waitForNameNodesContainer}
-}
-
-// buildVolumes builds volumes for DataNode StatefulSet
-func (b *DataNodeStatefulSetBuilder) buildVolumes() []corev1.Volume {
-	volumes := []corev1.Volume{
+	return []corev1.Volume{
 		{
 			Name: hdfsv1alpha1.HdfsConfigVolumeMountName,
 			VolumeSource: corev1.VolumeSource{
@@ -248,13 +130,6 @@ func (b *DataNodeStatefulSetBuilder) GetVolumeClaimTemplates() []corev1.Persiste
 	}
 }
 
-// GetSecurityContext returns the security context for datanode pods
-func (b *DataNodeStatefulSetBuilder) GetSecurityContext() *corev1.PodSecurityContext {
-	// For now, return nil as we'll handle security context in a later iteration
-	// This follows the pattern from namenode where security context is handled differently
-	return nil
-}
-
 // GetServiceAccountName returns the service account name for datanode
 func (b *DataNodeStatefulSetBuilder) GetServiceAccountName() string {
 	return common.CreateServiceAccountName(b.GetInstance().GetName())
@@ -277,13 +152,13 @@ func (b *DataNodeStatefulSetBuilder) makeWaitForNameNodesContainer() corev1.Cont
 		b.GetInstance(),
 		b.GetRoleGroupInfo(),
 		b.roleGroupConfig,
-		b.GetInstance().Spec.Image,
+		b.image,
 	)
 	return *waitForNameNodes.Build()
 }
 
 func (b *DataNodeStatefulSetBuilder) createDataPvcTemplate() corev1.PersistentVolumeClaim {
-	storageSize := b.roleGroupConfig.Resources.Storage.Capacity
+	storageSize := b.mergedCfg.Config.Resources.Storage.Capacity
 	return corev1.PersistentVolumeClaim{
 		ObjectMeta: metav1.ObjectMeta{
 			Name: hdfsv1alpha1.DataVolumeMountName,
@@ -332,268 +207,3 @@ func (b *DataNodeStatefulSetBuilder) getDataNodeConfigMapSource() *corev1.Config
 		},
 	}
 }
-
-// package data
-
-// import (
-// 	"context"
-
-// 	hdfsv1alpha1 "github.com/zncdatadev/hdfs-operator/api/v1alpha1"
-// 	"github.com/zncdatadev/hdfs-operator/internal/common"
-// 	"github.com/zncdatadev/hdfs-operator/internal/controller/data/container"
-// 	"github.com/zncdatadev/operator-go/pkg/constants"
-// 	"github.com/zncdatadev/operator-go/pkg/util"
-// 	appv1 "k8s.io/api/apps/v1"
-// 	corev1 "k8s.io/api/core/v1"
-// 	"k8s.io/apimachinery/pkg/api/resource"
-// 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-// 	"k8s.io/apimachinery/pkg/runtime"
-// 	"sigs.k8s.io/controller-runtime/pkg/client"
-// )
-
-// type StatefulSetReconciler struct {
-// 	common.WorkloadStyleReconciler[*hdfsv1alpha1.HdfsCluster, *hdfsv1alpha1.DataNodeRoleGroupSpec]
-// }
-
-// func NewStatefulSet(
-// 	scheme *runtime.Scheme,
-// 	instance *hdfsv1alpha1.HdfsCluster,
-// 	client client.Client,
-// 	groupName string,
-// 	labels map[string]string,
-// 	mergedCfg *hdfsv1alpha1.DataNodeRoleGroupSpec,
-// 	replicate int32,
-// 	image *util.Image,
-// ) *StatefulSetReconciler {
-// 	return &StatefulSetReconciler{
-// 		WorkloadStyleReconciler: *common.NewWorkloadStyleReconciler(
-// 			scheme,
-// 			instance,
-// 			client,
-// 			groupName,
-// 			labels,
-// 			mergedCfg,
-// 			replicate,
-// 			image,
-// 		),
-// 	}
-// }
-
-// func (s *StatefulSetReconciler) Build(ctx context.Context) (client.Object, error) {
-// 	sts := &appv1.StatefulSet{
-// 		ObjectMeta: metav1.ObjectMeta{
-// 			Name:      createStatefulSetName(s.Instance.GetName(), s.GroupName),
-// 			Namespace: s.Instance.GetNamespace(),
-// 			Labels:    s.MergedLabels,
-// 		},
-// 		Spec: appv1.StatefulSetSpec{
-// 			ServiceName: createServiceName(s.Instance.GetName(), s.GroupName),
-// 			Replicas:    s.getReplicates(),
-// 			Selector:    &metav1.LabelSelector{MatchLabels: s.MergedLabels},
-// 			Template: corev1.PodTemplateSpec{
-// 				ObjectMeta: metav1.ObjectMeta{
-// 					Labels: s.MergedLabels,
-// 				},
-// 				Spec: corev1.PodSpec{
-
-// 					ServiceAccountName: common.CreateServiceAccountName(s.Instance.GetName()),
-// 					SecurityContext:    s.MergedCfg.Config.SecurityContext,
-// 					Containers: []corev1.Container{
-// 						s.makeDataNodeContainer(),
-// 					},
-// 					InitContainers: []corev1.Container{
-// 						s.makeWaitNameNodeContainer(),
-// 					},
-// 					Volumes: s.makeVolumes(),
-// 				},
-// 			},
-// 			VolumeClaimTemplates: s.makePvcTemplates(),
-// 		},
-// 	}
-
-// 	isVectorEnabled, err := common.IsVectorEnable(s.MergedCfg.Config.Logging)
-// 	if err != nil {
-// 		return nil, err
-// 	} else if isVectorEnabled {
-// 		vectorFactory := common.GetVectorFactory(s.Image)
-// 		sts.Spec.Template.Spec.Containers = append(sts.Spec.Template.Spec.Containers, *vectorFactory.GetContainer())
-// 		sts.Spec.Template.Spec.Volumes = append(sts.Spec.Template.Spec.Volumes, vectorFactory.GetVolumes()...)
-// 	}
-// 	if s.Instance.Spec.ClusterConfig.Authentication != nil && s.Instance.Spec.ClusterConfig.Authentication.AuthenticationClass != "" {
-// 		oidcContainer, err := common.MakeOidcContainer(ctx, s.Client, s.Instance, s.getHttpPort(), s.Image)
-// 		if err != nil {
-// 			return nil, err
-// 		}
-// 		if oidcContainer != nil {
-// 			sts.Spec.Template.Spec.Containers = append(sts.Spec.Template.Spec.Containers, *oidcContainer)
-// 		}
-// 	}
-// 	return sts, nil
-// }
-
-// func (s *StatefulSetReconciler) getHttpPort() int32 {
-// 	return common.HttpPort(s.Instance.Spec.ClusterConfig, hdfsv1alpha1.DataNodeHttpsPort, hdfsv1alpha1.DataNodeHttpPort).ContainerPort
-// }
-
-// func (s *StatefulSetReconciler) SetAffinity(resource client.Object) {
-// 	dep := resource.(*appv1.StatefulSet)
-// 	if affinity := s.MergedCfg.Config.Affinity; affinity != nil {
-// 		dep.Spec.Template.Spec.Affinity = affinity
-// 	} else {
-// 		dep.Spec.Template.Spec.Affinity = common.AffinityDefault(common.DataNode, s.Instance.GetName())
-// 	}
-// }
-
-// func (s *StatefulSetReconciler) CommandOverride(resource client.Object) {
-// 	dep := resource.(*appv1.StatefulSet)
-// 	containers := dep.Spec.Template.Spec.Containers
-// 	if cmdOverride := s.MergedCfg.CliOverrides; cmdOverride != nil {
-// 		for i := range containers {
-// 			if containers[i].Name == string(container.DataNode) {
-// 				containers[i].Command = cmdOverride
-// 				break
-// 			}
-// 		}
-// 	}
-// }
-
-// func (s *StatefulSetReconciler) EnvOverride(resource client.Object) {
-// 	dep := resource.(*appv1.StatefulSet)
-// 	containers := dep.Spec.Template.Spec.Containers
-// 	if envOverride := s.MergedCfg.EnvOverrides; envOverride != nil {
-// 		for i := range containers {
-// 			if containers[i].Name == string(container.DataNode) {
-// 				envVars := containers[i].Env
-// 				common.OverrideEnvVars(&envVars, s.MergedCfg.EnvOverrides)
-// 				break
-// 			}
-
-// 		}
-// 	}
-// }
-
-// func (s *StatefulSetReconciler) LogOverride(_ client.Object) {
-// 	// do nothing, see name node
-// }
-
-// // make name node container
-// func (s *StatefulSetReconciler) makeDataNodeContainer() corev1.Container {
-// 	dateNode := container.NewDataNodeContainerBuilder(
-// 		s.Instance,
-// 		*common.ConvertToResourceRequirements(common.GetContainerResource(container.GetRole(), string(container.DataNode))),
-// 		s.Image,
-// 	)
-// 	return dateNode.Build(dateNode)
-// }
-
-// // make format name node container
-// func (s *StatefulSetReconciler) makeWaitNameNodeContainer() corev1.Container {
-// 	initContainer := container.NewWaitNameNodeContainerBuilder(
-// 		s.Instance,
-// 		*common.ConvertToResourceRequirements(common.GetContainerResource(container.GetRole(), string(container.WaitNameNode))),
-// 		s.GroupName,
-// 		s.Image,
-// 	)
-// 	return initContainer.Build(initContainer)
-// }
-
-// // make volumes
-// func (s *StatefulSetReconciler) makeVolumes() []corev1.Volume {
-// 	volumes := common.GetCommonVolumes(s.Instance.Spec.ClusterConfig, s.Instance.GetName(), container.GetRole())
-// 	datanodeVolumes := []corev1.Volume{
-// 		{
-// 			Name: hdfsv1alpha1.HdfsConfigVolumeMountName,
-// 			VolumeSource: corev1.VolumeSource{
-// 				ConfigMap: s.getConfigMapSource(),
-// 			},
-// 		},
-// 		{
-// 			Name: hdfsv1alpha1.HdfsLogVolumeMountName,
-// 			VolumeSource: corev1.VolumeSource{
-// 				ConfigMap: s.getConfigMapSource(),
-// 			},
-// 		},
-// 		{
-// 			Name: hdfsv1alpha1.WaitForNamenodesConfigVolumeMountName,
-// 			VolumeSource: corev1.VolumeSource{
-// 				ConfigMap: s.getConfigMapSource(),
-// 			},
-// 		},
-// 		{
-// 			Name: hdfsv1alpha1.WaitForNamenodesLogVolumeMountName,
-// 			VolumeSource: corev1.VolumeSource{
-// 				ConfigMap: s.getConfigMapSource(),
-// 			},
-// 		},
-// 		{
-// 			Name: hdfsv1alpha1.ListenerVolumeName,
-// 			VolumeSource: corev1.VolumeSource{
-// 				Ephemeral: &corev1.EphemeralVolumeSource{
-// 					VolumeClaimTemplate: s.createListenPvcTemplate(),
-// 				},
-// 			},
-// 		},
-// 	}
-// 	return append(volumes, datanodeVolumes...)
-// }
-
-// func (s *StatefulSetReconciler) makePvcTemplates() []corev1.PersistentVolumeClaim {
-// 	return []corev1.PersistentVolumeClaim{
-// 		s.createDataPvcTemplate(),
-// 	}
-// }
-
-// // create data pvc template
-// func (s *StatefulSetReconciler) createDataPvcTemplate() corev1.PersistentVolumeClaim {
-// 	storageSize := s.MergedCfg.Config.Resources.Storage.Capacity
-// 	return corev1.PersistentVolumeClaim{
-// 		ObjectMeta: metav1.ObjectMeta{
-// 			Name: hdfsv1alpha1.DataVolumeMountName,
-// 		},
-// 		Spec: corev1.PersistentVolumeClaimSpec{
-// 			AccessModes: []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
-// 			VolumeMode:  func() *corev1.PersistentVolumeMode { v := corev1.PersistentVolumeFilesystem; return &v }(),
-// 			Resources: corev1.VolumeResourceRequirements{
-// 				Requests: corev1.ResourceList{
-// 					corev1.ResourceStorage: storageSize,
-// 				},
-// 			},
-// 		},
-// 	}
-// }
-
-// // create listen pvc template
-// func (s *StatefulSetReconciler) createListenPvcTemplate() *corev1.PersistentVolumeClaimTemplate {
-// 	listenerClass := constants.ListenerClass(s.MergedCfg.Config.ListenerClass)
-// 	if listenerClass == "" {
-// 		listenerClass = constants.ClusterInternal
-// 	}
-// 	return &corev1.PersistentVolumeClaimTemplate{
-// 		ObjectMeta: metav1.ObjectMeta{
-// 			Annotations: common.GetListenerLabels(listenerClass),
-// 		},
-// 		Spec: corev1.PersistentVolumeClaimSpec{
-// 			StorageClassName: constants.ListenerStorageClassPtr(),
-// 			AccessModes:      []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
-// 			Resources: corev1.VolumeResourceRequirements{
-// 				Requests: corev1.ResourceList{
-// 					corev1.ResourceStorage: resource.MustParse(common.ListenerPvcStorage),
-// 				},
-// 			},
-// 		},
-// 	}
-// }
-
-// func (s *StatefulSetReconciler) getReplicates() *int32 {
-// 	return &s.MergedCfg.Replicas
-// }
-
-// func (s *StatefulSetReconciler) getConfigMapSource() *corev1.ConfigMapVolumeSource {
-// 	return &corev1.ConfigMapVolumeSource{
-// 		LocalObjectReference: corev1.LocalObjectReference{
-// 			Name: createConfigName(s.Instance.GetName(), s.GroupName)}}
-// }
-
-// func (s *StatefulSetReconciler) GetConditions() *[]metav1.Condition {
-// 	return &s.Instance.Status.Conditions
-// }
